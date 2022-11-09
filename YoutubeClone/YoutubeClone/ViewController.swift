5c89836c16875b3e6dbbbe51dcb9020261a74875
rodrigo <rodicristal17@gmail.com>
2022-11-08 23:46:19 -0300

    class_nro2

---
+++ YoutubeClone/YoutubeClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YoutubeClone
+//
+//  Created by Mac Mini on 2022-11-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
